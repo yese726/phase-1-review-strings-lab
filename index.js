384fd81f2552e7efd309448cdb7038547bc7eccf
Yesenia Urdiales <Yeseurdiales726@gmail.com>
2021-12-27 14:03:45 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+'Edsger Dijkstra'.length;
+//=> 15
+
+'Edsger Dijkstra'[15];
+//=> undefined
+
+'Edsger Dijkstra'[14];
+//=> "a"
+'Edsger Dijkstra'.slice(7);
+//=> "Edsger Dijkstra"
+'Edsger Dijkstra'.slice(0, 3);
+//=> "Eds"
+currentUser.slice(0, 1);
